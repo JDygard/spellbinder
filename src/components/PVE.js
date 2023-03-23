d642a005a0d62088e542d1254ba362ea26c7dbdf
joel.dygard <joel.dygard@synteda.se>
2023-03-23 20:27:31 +0100

    selecting challenges

--- src/components/PVE.js
+++ src/components/PVE.js
@@ -1,14 +1,49 @@
-import React, { useEffect } from 'react';
-import { getSocket, useSocket } from '../hooks/useSocketActions';
+import React, { useState, useEffect } from 'react';
+import { useSocket } from "../utils/SocketContext";
 import GameBoard from './GameBoard';
 
 const PVE = () => {
-  const handlePVEData = (data) => {
-    console.log("PVE data:", data);
+  const [challengeList, setChallengeList] = useState([]);
+  const [showGameBoard, setShowGameBoard] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const socket = useSocket();
+  
+  useEffect(() => {
+    socket.on('challengeList', (data) => {
+      console.log(data)
+      setChallengeList(data);
+      setIsLoading(false);
+    });
+
+    socket.emit('requestChallengeList');
+
+    return () => {
+      socket.off('challengeList');
+    };
+  }, [socket]);
+
+  const handleChallengeClick = () => {
+    setShowGameBoard(true);
   };
 
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
   return (
-    <GameBoard />
+    <>
+      {showGameBoard ? (
+        <GameBoard />
+      ) : (
+        <div>
+          {challengeList.map((challenge) => (
+            <button key={challenge.id} onClick={handleChallengeClick}>
+              {challenge.name}
+            </button>
+          ))}
+        </div>
+      )}
+    </>
   );
 };
 
